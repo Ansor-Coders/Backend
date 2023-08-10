7cc6e4ffddcbce2278cd515feb70247fb7370556
FarruxDEV <farruxbeknozimov@gmail.com>
2023-08-10 13:30:11 +0500

    connect to db

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,35 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { SequelizeModule } from '@nestjs/sequelize';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { resolve } from 'path';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ServeStaticModule.forRoot({
+      rootPath: resolve(__dirname, 'static'),
+    }),
+    ConfigModule.forRoot({ envFilePath: '.env', isGlobal: true }),
+    SequelizeModule.forRoot({
+      dialect: 'postgres',
+      host: process.env.PG_HOST,
+      port: Number(process.env.PG_PORT),
+      username: process.env.PG_USER,
+      password: String(process.env.PG_PASSWORD),
+      database: process.env.PG_DB,
+      autoLoadModels: true,
+      logging: false,
+      ssl: true,
+      dialectOptions: {
+        ssl: {
+          require: true,
+          rejectUnauthorized: false,
+        },
+      },
+      models: [],
+    }),
+  ],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
